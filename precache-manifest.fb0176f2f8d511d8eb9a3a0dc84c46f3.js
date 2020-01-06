4d64c1d5a12533157832f06d66b2f1d1f42ded15
Alexander Santos <alexsantosantana@live.com>
2020-01-05 21:28:28 -0500

    Updates

---
+++ precache-manifest.fb0176f2f8d511d8eb9a3a0dc84c46f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b62a22a58569a0d738478e45ec8a0e3",
+    "url": "/tibiacalculators/index.html"
+  },
+  {
+    "revision": "c293cd281703b8b75a6d",
+    "url": "/tibiacalculators/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "502f4b9f8cb5de79d5bb",
+    "url": "/tibiacalculators/static/css/main.cd534e5b.chunk.css"
+  },
+  {
+    "revision": "c293cd281703b8b75a6d",
+    "url": "/tibiacalculators/static/js/2.f93ff850.chunk.js"
+  },
+  {
+    "revision": "b480cab71264cd9c4f96b96a0330af52",
+    "url": "/tibiacalculators/static/js/2.f93ff850.chunk.js.LICENSE"
+  },
+  {
+    "revision": "502f4b9f8cb5de79d5bb",
+    "url": "/tibiacalculators/static/js/main.fa5f39e3.chunk.js"
+  },
+  {
+    "revision": "c2baa9d5ff8683d56c9d",
+    "url": "/tibiacalculators/static/js/runtime-main.9e396082.js"
+  }
+]);
